85ac812778435aec46dc926e1f23a09a2d864dd0
Marcel Moldaschl <m.moldaschl@adito.de>
2016-03-29 19:12:18 +0200

    added automatic configuration creation

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-${RUN}/run.sh
+
+if [ ! -f '/a/config/server.conf' ]
+then
+    gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /a/config/server.conf
+fi
+ln -sf /a/config/server.conf /etc/openvpn/
+
+
+if [ ! -f '/a/config/vars' ]
+then
+    cp /etc/openvpn/easy-rsa/vars.example /a/config/vars
+fi
+ln -sf /a/config/vars /etc/openvpn/easy-rsa/
 
 openvpn --config /etc/openvpn/server.conf
